7dc52b37bb670960178ef3cc01c0b517ba5e19c7
Oleksii Hytsiv <71726515+tarilo4ka@users.noreply.github.com>
2022-09-06 09:21:36 +0300

    Remove javadoc

--- src/main/java/com/intellias/hw8/HasArea.java
+++ src/main/java/com/intellias/hw8/HasArea.java
@@ -1,12 +1,6 @@
 package com.intellias.hw8;
 
-/**
- * The interface that implements <code>getArea()</code> method
- * @author Oleksii Hytsiv
- */
+
 public interface HasArea {
-    /**
-     * @return area of <code>Shape</code>
-     */
     double getArea();
 }
--- src/main/java/com/intellias/hw8/HasPerimeter.java
+++ src/main/java/com/intellias/hw8/HasPerimeter.java
@@ -1,12 +1,6 @@
 package com.intellias.hw8;
 
-/**
- * The interface that implements getArea() method
- * @author Oleksii Hytsiv
- */
+
 public interface HasPerimeter {
-    /**
-     * @return perimeter of <code>Shape</code>
-     */
     double getPerimeter();
 }
